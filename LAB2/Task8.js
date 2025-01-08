f2c1cd01dfef965f669d3390668b2b97ba158fd7
SharanyaRamidi2204 <girik019@gmail.com>
2025-01-08 10:31:28 +0530

    Task8 Destructuring of array

---
+++ LAB2/Task8.js
@@ -0,0 +1,10 @@
+const colors = ['red', 'green', 'blue', 'yellow'];
+const [fc, sc] = colors;
+
+console.log(fc);  
+console.log(sc); 
+const [, sec, , fourthc] = colors;
+
+console.log(sec); 
+console.log(fourthc); 
+
