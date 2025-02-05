abdfa3d52d69f6422e8aa839144c6e4919c2d01e
SharanyaRamidi2204 <girik019@gmail.com>
2025-02-05 10:16:07 +0530

    os module to display the system information

---
+++ LAB5/TASK12.js
@@ -0,0 +1,6 @@
+import os from 'os'
+console.log(`Host name: ${os.hostname()}`)
+console.log(`Platform: ${os.platform()}`)
+console.log(`Uptime: ${os.uptime()}`)
+console.log(`Total memory: ${os.totalmem()}`)
+console.log(`Free memory: ${os.freemem()}`)
